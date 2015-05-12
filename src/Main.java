b6bdc848dcdaf05e34410ee4883f9915f9604e46
Emmanuel Morfiadakis <u5587023@anu.edu.au>
2015-05-12 12:44:48 +1000

    Added timner

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.awt.AWTException;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.HeadlessException;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +15,7 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 
 public class Main {
@@ -26,14 +30,43 @@ public class Main {
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.setForeground(Color.BLACK);
        frame.setVisible(true);
-	}
-	
+       while(true){
+       int z = randomWithRange(0,30*1000);	// 0 - 30 seconds
+       System.out.println(z/1000);			//when the pic is taken
+       
+    
+		final Timer timer = new Timer(z, new ActionListener() {
+		
 
-	public static void main(String[] args) throws AWTException, IOException  {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				try {
+					takeScr();
+				} catch (HeadlessException | IOException | AWTException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+			}
+		});
+		timer.start();}
+
+	}
+	private static void takeScr() throws IOException, HeadlessException, AWTException{
 		BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
 		ImageIO.write(image, "png", new File(System.getProperty("user.dir")
-				+ "/contact.png" +
+				+ "/screenshot.png" +
 				""));
+	}
+	
+	static int randomWithRange(int min, int max)
+	{
+	   int range = (max - min) + 1;     
+	   return (int)(Math.random() * range) + min;
+	}
+	
+	public static void main(String[] args) throws AWTException, IOException  {
+	
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 
 			@Override
